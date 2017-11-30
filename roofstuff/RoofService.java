958b701dbaeede9cef77d54602457a21d6a53f15
Michael Nening <mnening@cosy.sbg.ac.at>
2017-11-30 16:39:01 +0100

    stuff and things

--- roofstuff/RoofService.java
+++
@@ -1,8 +0,0 @@
-package com.example.service;
-
-import com.example.model.Roof;
-
-public interface RoofService {
-  public Roof findRoofByAddress(String address);
-  public void saveRoof(Roof roof);
-}
