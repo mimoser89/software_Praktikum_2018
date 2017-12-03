87d9fa7e0f9bfbfc03f61d1d81061ce58654687e
Michael Nening <mnening@cosy.sbg.ac.at>
2017-12-03 19:05:54 +0100

    clean up

--- roofStuff/coolRoofProject/src/main/java/com/example/service/InvestmentService.java
+++
@@ -1,9 +0,0 @@
-package com.example.service;
-
-import com.example.model.Investment;
-import java.util.List;
-
-public interface InvestmentService {
-	public List<Investment> findAll();
-	public List<Investment> findByuserId(int userId);
-}
